a938b7a2dfb074d3c73d1c815232d819cad66c81
Sam Protsenko <semen.protsenko@linaro.org>
2019-05-16 00:59:53 +0300

    Revert "fastboot: getvar: correct/rename "has_slot" to "has-slot""
    When using fastboot to flash boot partition, next result observed:
    
    Sending 'boot__a' (11395 KB)       OKAY [  0.423s]
    Writing 'boot__a'                  FAILED (remote: 'cannot find partition')
    fastboot: error: Command failed
    
    Most likely, we should rename our Android partitions, by adding _a
    suffix.
    
    "git bisect" found that regression is this commit. Let's revert it for
    now, before we figure out how to handle it properly.
    
    This reverts commit 4c829466fcc8c0b5303af6bda21366657b959a30.

--- drivers/fastboot/fb_getvar.c
+++ drivers/fastboot/fb_getvar.c
@@ -66,7 +66,7 @@ static const struct {
 		.variable = "slot-suffixes",
 		.dispatch = getvar_slot_suffixes
 	}, {
-		.variable = "has-slot",
+		.variable = "has_slot",
 		.dispatch = getvar_has_slot
 #if CONFIG_IS_ENABLED(FASTBOOT_FLASH_MMC)
 	}, {
